0332e2035cbce147f7c9bab2c5784c7e8203c6dc
bmw <narayananani1806@gmail.com>
2026-09-18 23:46:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(
+    __name__,
+    static_folder="static",
+    template_folder="templates"
+)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
